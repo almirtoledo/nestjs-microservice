0df9bba5526d5919a7a61b659e397c56d115e9d6
Almir Toledo <deendsstudios@gmail.com>
2024-09-11 11:17:37 -0300

    feat: implements validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.KAFKA,
-      options: {
-        client: {
-          brokers: [process.env.KAFKA_BROKER],
-        },
-        consumer: {
-          groupId: process.env.KAFKA_GROUP_ID,
-        },
-      },
-    },
-  );
-  await app.listen();
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   {
+  //     transport: Transport.KAFKA,
+  //     options: {
+  //       client: {
+  //         brokers: [process.env.KAFKA_BROKER],
+  //       },
+  //       consumer: {
+  //         groupId: process.env.KAFKA_GROUP_ID,
+  //       },
+  //     },
+  //   },
+  // );
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: '*' });
+  app.use(new ValidationPipe());
+
+  await app.listen(3000);
 }
 bootstrap();
